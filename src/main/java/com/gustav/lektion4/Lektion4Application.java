a2c32958b9d3a64749d530c1fd4caa18b7288ca4
gustavnybergs <Gustavnybergs@gmail.com>
2025-08-27 21:23:19 +0200

    init: Skapar nytt Spring Boot-projekt för lektion 4 (Spring Web)

---
+++ src/main/java/com/gustav/lektion4/Lektion4Application.java
@@ -0,0 +1,13 @@
+package com.gustav.lektion4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lektion4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lektion4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gustav/lektion4/Lektion4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustav.lektion4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lektion4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
