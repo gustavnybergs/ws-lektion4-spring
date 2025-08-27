d9e9830ae69aa689742a8c0c5875d0278dc28a67
gustavnybergs <Gustavnybergs@gmail.com>
2025-08-27 21:36:22 +0200

    feat: Lägger till GET /users/{username} med @PathVariable

--- src/main/java/com/gustav/lektion4/controller/UserController.java
+++ src/main/java/com/gustav/lektion4/controller/UserController.java
@@ -3,11 +3,20 @@ package com.gustav.lektion4.controller;
 import com.gustav.lektion4.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class UserController {
+
     @GetMapping("/newUser")
     public User getNewUser() {
         return new User(0, "Benny", "123", true);
     }
+
+    @GetMapping("/users/{username}")
+    public User getByUsername(@PathVariable String username) {
+        return new User(0, username, "123", true);
+    }
 }
+
+
